b7f34d76e46f08c42b65a5f4dc65d628c9f5c0a7
asbahere <98083895+asbahere@users.noreply.github.com>
2022-08-08 12:29:45 +0530

    Update knap.c

--- knap.c
+++ knap.c
@@ -1,35 +1,87 @@
-#include <stdio.h>
-int max(int a, int b) { return (a > b)? a : b; }
-int knapsack(int capacity, int wt[], int val[], int n)
+#include<stdio.h>
+#include<conio.h>
+void knapsack();
+int max(int,int);
+int i,j,n,m,p[10],w[10],v[10][10];
+void main()
 {
-   int i, w;
-   int K[n+1][capacity+1];
-   for (i = 0; i <=n; i++)// 0,1  n=5
-   {
-       for (w = 0; w <=capacity; w++)// 0,1
-       {
-           if (i==0 || w==0)
-               K[i][w] = 0;
-           else if (wt[i-1] <= w)
-            K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],K[i-1][w]);
-           else
-                 K[i][w] = K[i-1][w];
-       }
-   }
 
-   return K[n][capacity];
+ printf("\nenter the no. of items:\t");
+ scanf("%d",&n);
+ printf("\nenter the weight of the each item:\n");
+ for(i=1;i<=n;i++)
+ {
+  scanf("%d",&w[i]);
+ }
+ printf("\nenter the profit of each item:\n");
+ for(i=1;i<=n;i++)
+ {
+  scanf("%d",&p[i]);
+ }
+ printf("\nenter the knapsack's capacity:\t");
+ scanf("%d",&m);
+ knapsack();
+ getch();
 }
 
-int main()
+void knapsack()
 {
-    int val[100],wt[100],capacity,n,i;
-    printf("Enter the no of items:\n");
-    scanf("%d",&n);
-    printf("Enter the weight and value of items:\n");
-    for(i=0;i<n;i++)
-    scanf("%d%d",&wt[i], &val[i]);
-    printf("Enter the knapsack size:\n");
-    scanf("%d",&capacity);
-    printf("\nValue = %d", knapsack(capacity, wt, val, n));
-    return 0;
+ int x[10];
+ for(i=0;i<=n;i++)
+ {
+  for(j=0;j<=m;j++)
+  {
+   if(i==0||j==0)
+   {
+    v[i][j]=0;
+   }
+   else if(j-w[i]<0)
+   {
+    v[i][j]=v[i-1][j];
+   }
+   else
+   {
+    v[i][j]=max(v[i-1][j],v[i-1][j-w[i]]+p[i]);
+   }
+  }
+ }
+ printf("\nthe output is:\n");
+ for(i=0;i<=n;i++)
+ {
+  for(j=0;j<=m;j++)
+  {
+   printf("%d\t",v[i][j]);
+  }
+  printf("\n\n");
+ }
+ printf("\nthe optimal solution is %d",v[n][m]);
+ printf("\nthe solution vector is:\n");
+ for(i=n;i>=1;i--)
+ {
+  if(v[i][m]!=v[i-1][m])
+  {
+   x[i]=1;
+   m=m-w[i];
+  }
+  else
+  {
+   x[i]=0;
+  }
+ }
+ for(i=1;i<=n;i++)
+ {
+  printf("%d\t",x[i]);
+ }
+}
+
+int max(int x,int y)
+{
+ if(x>y)
+ {
+  return x;
+ }
+ else
+ {
+  return y;
+ }
 }
